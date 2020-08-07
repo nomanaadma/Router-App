433b511ffeb2ac814a64240bf57e1fb31396ee2a
Noman Shoukat <nomanaadma@gmail.com>
2020-08-07 17:37:25 +0500

    Product  Details Routing with Param

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-          <Route path="/products" component={Products} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/admin" component={Dashboard} />
-          <Route path="/" component={Home} />
-          {/* <Route path="/" exact component={Home} /> */}
+            <Route path="/products/:id" component={ ProductDetails } />
+            <Route path="/products" render={ props => <Products sortBy="newest" {...props} /> } />
+            <Route path="/posts/:year/:months" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
           </Switch>
         </div>
       </div>

110d4bd2dd0b5341c066c0fe1dcd43dc3fa08660
Noman Shoukat <nomanaadma@gmail.com>
2020-08-07 17:53:05 +0500

    Redirecting Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -20,7 +20,10 @@ class App extends Component {
             <Route path="/products" render={ props => <Products sortBy="newest" {...props} /> } />
             <Route path="/posts/:year?/:months?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Redirect from="/messages" to="/posts" />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
